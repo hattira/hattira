6d4a4a8e1e3622b6dea3d56a993d901dfd71e44e
Pradip P Caulagi <caulagi@gmail.com>
2013-07-26 18:04:25 +0530

    Show landing page

--- config/routes.js
+++ config/routes.js
@@ -36,41 +36,12 @@ module.exports = function (app, passport) {
       failureFlash: 'Invalid email or password.'
     }), users.session)
   app.get('/users/:userId', users.show)
-  app.get('/auth/facebook',
-    passport.authenticate('facebook', {
-      scope: [ 'email', 'user_about_me'],
-      failureRedirect: '/login'
-    }), users.signin)
-  app.get('/auth/facebook/callback',
-    passport.authenticate('facebook', {
-      failureRedirect: '/login'
-    }), users.authCallback)
-  app.get('/auth/github',
-    passport.authenticate('github', {
-      failureRedirect: '/login'
-    }), users.signin)
-  app.get('/auth/github/callback',
-    passport.authenticate('github', {
-      failureRedirect: '/login'
-    }), users.authCallback)
   app.get('/auth/twitter',
     passport.authenticate('twitter', {
       failureRedirect: '/login'
     }), users.signin)
   app.get('/auth/twitter/callback',
     passport.authenticate('twitter', {
-      failureRedirect: '/login'
-    }), users.authCallback)
-  app.get('/auth/google',
-    passport.authenticate('google', {
-      failureRedirect: '/login',
-      scope: [
-        'https://www.googleapis.com/auth/userinfo.profile',
-        'https://www.googleapis.com/auth/userinfo.email'
-      ]
-    }), users.signin)
-  app.get('/auth/google/callback',
-    passport.authenticate('google', {
       failureRedirect: '/login'
     }), users.authCallback)
 
@@ -88,7 +59,7 @@ module.exports = function (app, passport) {
   app.param('id', articles.load)
 
   // home route
-  app.get('/', articles.index)
+  app.get('/', auth.requiresLogin, articles.index)
 
   // comment routes
   var comments = require('../app/controllers/comments')
