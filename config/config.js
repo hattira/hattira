0141552af9d0e27205d2e3f4720edb940867f3e4
Pradip P Caulagi <caulagi@gmail.com>
2013-08-02 17:43:49 +0530

    Keep it DRY

--- config/config.js
+++ config/config.js
@@ -1,46 +1,34 @@
 
-var path = require('path')
-  , rootPath = path.normalize(__dirname + '/..')
-  , templatePath = path.normalize(__dirname + '/../app/mailer/templates')
+var rootPath = require('path').normalize(__dirname + '/..')
+  , _ = require('underscore')
+
+var _base = {
+  db: 'mongodb://localhost/deau',
+  root: rootPath,
+  app: {
+    name: 'deau: Developer Events Around You'
+  },
+  twitter: {
+    clientID: "j9X8kVVckZIjepCH9G2zNQ",
+    clientSecret: "YdVw6jciDdVzAIeptYjo0ZTIHqIeXXccordtb0fmm6U",
+    callbackURL: "http://localhost:3000/auth/twitter/callback"
+  },
+  MIXPANEL_ID: process.env.MIXPANEL_ID || "86d6a0a2e95c442691e4dc5543dbc833"
+}
+
+var development = _.extend({}, _base, { db: _base.db+'_dev' })
+  , test        = _.extend({}, _base, { db: _base.db+'_test' })
+  , production  = _.extend({}, _base, {
+      db: process.env.MONGOHQ_URL,
+      twitter: {
+        clientID: process.env.TWITTER_CONSUMER_KEY,
+        clientSecret: process.env.TWITTER_CONSUMER_SECRET,
+        callbackURL: "http://deau.pw/auth/twitter/callback"
+      }
+    })
 
 module.exports = {
-  development: {
-    db: 'mongodb://localhost/deau_dev',
-    root: rootPath,
-    app: {
-      name: 'deau: Developer Events Around You'
-    },
-    twitter: {
-      clientID: "j9X8kVVckZIjepCH9G2zNQ",
-      clientSecret: "YdVw6jciDdVzAIeptYjo0ZTIHqIeXXccordtb0fmm6U",
-      callbackURL: "http://localhost:3000/auth/twitter/callback"
-    },
-    MIXPANEL_ID: "86d6a0a2e95c442691e4dc5543dbc833"
-  },
-  test: {
-    db: 'mongodb://localhost/deau_test',
-    root: rootPath,
-    app: {
-      name: 'deau: Developer Events Around You'
-    },
-    twitter: {
-      clientID: "CONSUMER_KEY",
-      clientSecret: "CONSUMER_SECRET",
-      callbackURL: "http://localhost:3000/auth/twitter/callback"
-    },
-    MIXPANEL_ID: "86d6a0a2e95c442691e4dc5543dbc833"
-  },
-  production: {
-    db: process.env.MONGOHQ_URL || 'mongodb://localhost/deau_prod',
-    root: rootPath,
-    app: {
-      name: 'deau: Developer Events Around You'
-    },
-    twitter: {
-      clientID: process.env.TWITTER_CONSUMER_KEY,
-      clientSecret: process.env.TWITTER_CONSUMER_SECRET,
-      callbackURL: "http://deau.pw/auth/twitter/callback"
-    },
-    MIXPANEL_ID: process.env.MIXPANEL_ID || "86d6a0a2e95c442691e4dc5543dbc833"
-  }
+  development: development,
+  test: test,
+  production: production
 }
