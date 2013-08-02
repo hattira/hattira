f38bea4e666fc777660ff6f842610d0552486e92
Pradip P Caulagi <caulagi@gmail.com>
2013-08-02 11:18:56 +0530

    Allow users to browse by tags

--- app/controllers/tags.js
+++ app/controllers/tags.js
@@ -12,21 +12,35 @@ var mongoose = require('mongoose')
 
 exports.index = function (req, res) {
   var criteria = { tags: req.param('tag') }
-  var perPage = 5
-  var page = req.param('page') > 0 ? req.param('page') : 0
-  var options = {
-    perPage: perPage,
-    page: page,
-    criteria: criteria
-  }
+    , perPage = 5
+    , page = req.param('page') > 0 ? req.param('page') : 0
+    , tags = []
+    , options = {
+        perPage: perPage,
+        page: page,
+        criteria: criteria
+      }
 
   Meetup.list(options, function(err, meetups) {
-    if (err) return res.render('500')
+    if (err) {
+      return res.render('500')
+    }
+
+    meetups.forEach(function(meetup, index) {
+      meetup.tags.split(',').forEach(function (tag, index) {
+        tag = tag.trim()
+        if (tag && !tags[tag]) {
+          tags.push(tag)
+        }
+      })
+    })
+
     Meetup.count(criteria).exec(function (err, count) {
       res.render('meetups/index', {
         title: 'Meetups tagged ' + req.param('tag'),
         meetups: meetups,
         page: page,
+        tags: tags,
         pages: count / perPage
       })
     })
