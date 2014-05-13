087f36dc6a466e47a4c9a04f637dc525d3c73df8
Pradip P Caulagi <caulagi@gmail.com>
2014-05-13 07:41:21 +0530

    Remove filtering by location when showing tags

--- app/controllers/tags.js
+++ app/controllers/tags.js
@@ -15,7 +15,6 @@ exports.index = function (req, res, next) {
     }
     return meetups.renderMeetups(res, results, {
       title: util.format("Meetups with tag: %s", req.param("tag")),
-      loc: req.session["loc"]
     })
   })
 }
