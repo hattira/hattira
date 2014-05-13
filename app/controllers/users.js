e068cf93c554d064aafd9bc12864e19bbfd4e41e
Pradip P Caulagi <caulagi@gmail.com>
2014-05-13 10:12:30 +0530

    Don't throw an error if user not found?

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -90,8 +90,9 @@ exports.user = function (req, res, next, id) {
     .findOne({ _id : id })
     .exec(function (err, user) {
       if (err) return next(err)
-      if (!user) return next(new Error('Failed to load User ' + id))
-      req.profile = user
+      if (user) {
+        req.profile = user
+      }
       next()
     })
 }
