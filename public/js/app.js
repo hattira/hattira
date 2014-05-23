0704d6be18eb3162e1073ce80cfbdfa30be9f7f2
Pradip P Caulagi <caulagi@gmail.com>
2014-05-23 13:07:22 +0530

    Dead code

--- public/js/app.js
+++ public/js/app.js
@@ -24,8 +24,6 @@ function showVenueMap(address) {
 $(document).ready(function () {
   $('#startDate').datetimepicker();
   $('#endDate').datetimepicker();
-  $('#marketing').carousel({interval: 5000})
-
   $('#attending').submit(function(e) {
     var id = $(this.meetup_id).val()
       , csrf = $(this._csrf).val()
@@ -44,28 +42,6 @@ $(document).ready(function () {
           location.reload()
         } else {
           node.addClass('alert-danger')
-        }
-      }
-    })
-  })
-
-  $('#share').submit(function(e) {
-    var id = $(this.meetup_id).val()
-      , csrf = $(this._csrf).val()
-    e.preventDefault();
-
-    $.ajax({
-      url: '/meetups/'+id+'/share',
-      type: 'PUT',
-      data: {_csrf: csrf},
-      dataType: "json",
-      success: function(result) {
-        var node = $('#feedback-box')
-        node.removeClass('hide')
-        if (result.status === 'ok') {
-          node.html("Updated Facebook successfully").addClass('alert-success')
-        } else {
-          node.html(result.message.message).addClass('alert-danger')
         }
       }
     })
