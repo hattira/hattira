7637a7efce148bab7123291ed027db7517d6bc07
Pradip P Caulagi <caulagi@gmail.com>
2015-01-02 21:27:46 +0530

    Fix tests

--- server/routes.js
+++ server/routes.js
@@ -10,7 +10,6 @@ module.exports = function(app) {
 
   // Insert routes below
   app.use('/api/meetups', require('./api/meetup'));
-  app.use('/api/things', require('./api/thing'));
   app.use('/api/users', require('./api/user'));
 
   app.use('/auth', require('./auth'));
